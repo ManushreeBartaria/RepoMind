270772ce54c1faa2015ccd7905b83516b8201ba6
unknown <manushreebartaria07@gmail.com>
2026-01-17 01:13:32 +0530

    ast_parser function added and chunks of python files are getting created

--- ingestion.py
+++ ingestion.py
@@ -1,6 +1,7 @@
 import os
 import git
 from pathlib import Path
+import ast
 
 def cloning(git_link):
     repo_name = git_link.split("/")[-1].replace(".git", "")
@@ -13,14 +14,12 @@ def clean_files(path: Path):
     ".git", "venv", "__pycache__", "node_modules",
     "dist", "build", ".cache"
 }
-
     IGNORED_EXTENSIONS = {
         ".png", ".jpg", ".jpeg", ".gif", ".svg",
         ".mp3", ".mp4", ".wav",
         ".pdf", ".zip", ".exe", ".bin",
         ".env", ".lock", ".log", ".csv"
     }
-
     IGNORED_FILES = {
         ".gitignore","__init__.py"
     }
@@ -39,19 +38,50 @@ def clean_files(path: Path):
     return cleaned_files
 
 def send_to_parser(cleaned_files):
+    py_files=[]
     for file in cleaned_files:
         extension=file.suffix.lower()
         if extension==".py":
-            ast_parser(file)
+            py_files.append(file)
+    return py_files
 
 def ast_parser(file_path: Path):
-    pass
-        
+    chunks=[]
+    try:
+        with open(file_path, "r", encoding="utf-8") as file:
+            file_content = file.read()
+        tree = ast.parse(file_content)
+    except Exception as e:
+            print(f"Error parsing {file_path}: {e}")
+    for node in ast.walk(tree):
+        if isinstance(node, (ast.FunctionDef, ast.ClassDef, ast.AsyncFunctionDef)):
+            code=ast.get_source_segment(file_content, node)
+            chunks.append({
+                "name": node.name,
+                "type": type(node).__name__,
+                "code":code,
+                "start_line": node.lineno,
+                "end_line": node.end_lineno,
+                })
+    if not chunks:
+        chunks.append({
+                "name": file_path.stem,
+                "type": "Module",
+                "code": file_content,
+                "start_line": 1,
+                "end_line": len(file_content.splitlines()),
+                })
+    return chunks       
+
         
         
 if __name__ == "__main__":
     file_name=cloning("https://github.com/ManushreeBartaria/TrickIT.git")
     file_path="cloned_files/"+file_name
     cleaned_files=clean_files(Path(file_path))
-    send_to_parser(cleaned_files)
+    files=send_to_parser(cleaned_files)
+    chunks=[]
+    for file in files:
+       chunks=ast_parser(file)
+       print("total chunks:",len(chunks))  
 
