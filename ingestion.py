c78cdf3bcbf42f93b95b20893921df8047bdc68a
unknown <manushreebartaria07@gmail.com>
2026-01-16 10:02:56 +0530

    bug in cloning func resolved

--- ingestion.py
+++ ingestion.py
@@ -2,8 +2,8 @@ import os
 import git
 def cloning(git_link):
     repo_name = git_link.split("/")[-1].replace(".git", "")
-    if not os.path.exists(repo_name):
-        git.Repo.clone_from(git_link, repo_name)
+    if not os.path.exists("cloned_files/" + repo_name):
+        git.Repo.clone_from(git_link, "cloned_files/" + repo_name)
     return repo_name
 if __name__ == "__main__":
-    cloning("https://github.com/ManushreeBartaria/ContactBook.git")
+    cloning("https://github.com/ManushreeBartaria/TrickIT.git")
